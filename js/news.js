cfb6387ccc7e39bde308c61aa670b5afb5663bf8
Christian Paling <christian.paling@googlemail.com>
2017-04-08 18:02:00 +0200

    Add form to subscribe to newsletter

--- js/news.js
+++ js/news.js
@@ -3,9 +3,46 @@
     url: 'http://www.homoeopathie-paling.de/_news.html',
     method: 'GET',
     cors: true },
-    (code, response) => {
+    function(code, response) {
       var news = document.getElementById('news');
       news.innerHTML = response;
     }
   );
+
+  var form = document.querySelector('#form form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var firstname = document.querySelector('input[name="firstname"]').value;
+    var lastname = document.querySelector('input[name="lastname"]').value;
+    var email = document.querySelector('input[name="email"]').value;
+
+    var json = {
+      firstname: firstname,
+      lastname: lastname,
+      email: email
+    };
+
+    var url = location.protocol === 'file:' ? 'http://localhost:3000/api/subscribers' : 'http://newsletter.homoeopathie-paling.de/api/subscribers';
+
+    nanoajax.ajax({
+      url: url,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(json),
+      cors: true },
+      function(code, resp) {
+        if (code === 201) {
+          document.querySelector('p.error').style = 'display: none';
+          document.querySelector('p.success').style = 'display: block';
+        }
+        else {
+          document.querySelector('p.success').style = 'display: none';
+          document.querySelector('p.error').style = 'display: block';
+        }
+      }
+    );
+  });
 }());
